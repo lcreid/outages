f9e9eb15ea848b3401d69121bf2671f53d1b8b5c
Larry Reid <lcreid@jadesystems.ca>
2016-02-10 21:02:38 -0600

    Make it run.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks//= require react
-//= require react_ujs
-//= require components
+//= require turbolinks
 
 // For React https://github.com/reactjs/react-rails
 //= require react
