bd634752b85ad13507cdba0a900388d6bd78a509
Larry Reid <lcreid@jadesystems.ca>
2016-02-21 19:10:47 -0800

    Combine data and time before storing.

--- app/controllers/outages_controller.rb
+++ app/controllers/outages_controller.rb
@@ -14,10 +14,10 @@ class OutagesController < ApplicationController
 
   def create
     # render plain: params[:outage].inspect
+    # Combine dates and times.
+    params[:outage][:start_time] = combine(params[:outage][:start_date], params[:outage][:start_time])
+    params[:outage][:end_time] = combine(params[:outage][:end_date], params[:outage][:end_time])
     @outage = Outage.new(outage_params)
-
-    # TODO: Combine dates and times.
-    # TODO: Use time zone
 
     @outage.save
     redirect_to @outage
@@ -32,5 +32,9 @@ class OutagesController < ApplicationController
   private
   def outage_params
     params[:outage].permit(:title, :start_time, :end_time, :time_zone, :description)
+  end
+
+  def combine(date, time)
+    date.strip + "T" + time.strip
   end
 end
--- app/models/outage.rb
+++ app/models/outage.rb
@@ -1,9 +1,14 @@
 class Outage < ApplicationRecord
   def start_time_utc
-    ActiveSupport::TimeZone[time_zone].parse(start_time)
+    parse(start_time)
   end
 
   def end_time_utc
-    ActiveSupport::TimeZone[time_zone].parse(end_time)
+    parse(end_time)
+  end
+
+  private
+  def parse(s)
+    ActiveSupport::TimeZone[time_zone].parse(s)
   end
 end
