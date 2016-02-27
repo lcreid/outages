f96f4cfd82bf9d1125959894e3e68f80936734be
Larry Reid <lcreid@jadesystems.ca>
2016-02-27 16:11:14 -0600

    Fix errors in time zone conversion.
    For some reason, my test case worked even though I wasn't doing UTC right.
    Need to look at the test cases a bit more.
    
    Also refactored to get some duplication out. I need to learn more Ruby Fu.

--- app/models/outage.rb
+++ app/models/outage.rb
@@ -1,10 +1,24 @@
 class Outage < ApplicationRecord
   def start_datetime_utc
-    parse(start_datetime_s)
+    parse(start_datetime_s).utc
   end
 
   def end_datetime_utc
-    parse(end_datetime_s)
+    parse(end_datetime_s).utc
+  end
+
+  def start_datetime_tz(tz)
+    # puts "UTC: " + start_datetime_utc.to_s
+    # puts tz.name
+
+    to_tz(tz).utc_to_local(start_datetime_utc)
+  end
+
+  def end_datetime_tz(tz)
+    # puts "UTC: " + end_datetime_utc.to_s
+    # puts tz.name
+
+    to_tz(tz).utc_to_local(end_datetime_utc)
   end
 
   private
@@ -18,5 +32,13 @@ class Outage < ApplicationRecord
 
   def end_datetime_s
     end_date + "T" + end_time
+  end
+
+  def to_tz(tz)
+    return ActiveSupport::TimeZone[tz] if tz.class == String
+    tz
+  end
+
+  def datetime_tz(tz)
   end
 end
--- test/models/outage_test.rb
+++ test/models/outage_test.rb
@@ -10,6 +10,16 @@ class OutageTest < ActiveSupport::TestCase
     assert_equal Time.utc(2015, 12, 31, 10, 0, 1), o.end_datetime_utc
   end
 
+  test "happy new year samoa from vancouver" do
+    o = outages(:happy_new_year_samoa)
+    tz_name = "Pacific Time (US & Canada)"
+    tz = ActiveSupport::TimeZone.new(tz_name)
+    assert_equal Time.utc(2015, 12, 31, 2, 0, 0), o.start_datetime_tz(tz)
+    assert_equal Time.utc(2015, 12, 31, 2, 0, 1), o.end_datetime_tz(tz)
+    assert_equal Time.utc(2015, 12, 31, 2, 0, 0), o.start_datetime_tz(tz_name)
+    assert_equal Time.utc(2015, 12, 31, 2, 0, 1), o.end_datetime_tz(tz_name)
+  end
+
   test "happy new year london" do
     o = outages(:happy_new_year_london)
     assert_equal Time.utc(2016, 1, 1, 0, 0, 0), o.start_datetime_utc
